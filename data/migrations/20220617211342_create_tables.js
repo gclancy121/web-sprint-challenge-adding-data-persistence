fc584a19a9946c9aedeecbfcd0786a75aec66e7a
Gage Clancy <gclancy121@gmail.com>
2022-06-17 16:37:32 -0500

    database created

--- data/migrations/20220617211342_create_tables.js
+++ data/migrations/20220617211342_create_tables.js
@@ -1,8 +1,34 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects', tbl => {
+    tbl.increments('project_id');
+    tbl.string('project_name', 128).unique().notNullable();
+    tbl.string('project_description', 128);
+    tbl.boolean('project_completed').defaultTo(0);
+  })
+  .createTable('resources', tbl => {
+    tbl.increments('resource_id');
+    tbl.string('resource_name', 128).unique().notNullable();
+    tbl.string('resource_description', 256);
+  })
+  .createTable('tasks', tbl => {
+    tbl.increments('task_id');
+    tbl.string('task_description, 128').notNullable();
+    tbl.string('task_notes', 128);
+    tbl.boolean('task_completed').defaultTo(0);
+    tbl.integer('project_id')
+    .unsigned()
+    .notNullable()
+    .references('project_id')
+    .inTable('projects');
+  })
 };
 
 exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('tasks')
+  .dropTableIfExists('resources')
+  .dropTableIfExists('projects');
   
 };
