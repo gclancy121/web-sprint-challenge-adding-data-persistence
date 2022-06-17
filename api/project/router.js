eca3348f55a35b7a6be0dec9bc7199072fb13c7a
Gage Clancy <gclancy121@gmail.com>
2022-06-17 16:22:19 -0500

    all initial setup complete

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,18 @@
-// build your `/api/projects` router here
+// build your `/api/tasks` router here
+const express = require('express');
+//const Project = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.json({message: 'Project hookup complete chief!'})
+});
+
+
+
+
+
+
+
+
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,18 @@
-// build your `/api/resources` router here
+// build your `/api/tasks` router here
+const express = require('express');
+//const Resource = require('./model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.json({message: 'Resource hookup complete chief!'})
+});
+
+
+
+
+
+
+
+
+module.exports = router;
